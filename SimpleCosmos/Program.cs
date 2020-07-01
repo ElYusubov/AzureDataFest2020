b80aa43b3a6c9c1e437785679c1f0517ee4d363d
ElYusubov <elxany@hotmail.com>
2020-07-01 17:59:00 -0400

    Add exception handling and other good practices

--- SimpleCosmos/Program.cs
+++ SimpleCosmos/Program.cs
@@ -9,16 +9,34 @@ namespace Cosmos.Utility
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello Azure Data Fest 2020 Attendees!");
-
             string endpoint = "'your-cosmosdb-host'";
             string authKey = "'your-unique-key-from-cosmosdb'";
             
-            CosmosClient client = new CosmosClient(endpoint, authKey);
-            await client.ReadAccountAsync();
+            try
+            {
+                using (CosmosClient client = new CosmosClient(endpoint, authKey))
+                {
+                    await client.ReadAccountAsync();
+                }
+                Console.WriteLine();
+                Console.WriteLine("Read account operation succeeded.");
+                 Console.WriteLine();
+            }
+            catch(CosmosException cosex)
+            {
+                Console.WriteLine("Cosmos Exception Message: {0}", cosex.ToString());
+            }
+            catch(Exception ex)
+            {
+                Exception baseException = ex.GetBaseException();
+                Console.WriteLine("Base Exception: {0}, Message: {1}", ex.Message, baseException.Message);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
 
-
-            Console.WriteLine("End of demo, press any key to exit.");
-            Console.ReadKey();
         }
     }
 }
