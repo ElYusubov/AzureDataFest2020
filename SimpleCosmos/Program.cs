001763cc0cda5656cb8930240b482db221f74f7f
ElYusubov <elxany@hotmail.com>
2020-07-01 17:47:52 -0400

    Add SimpleCosmos demo app

---
+++ SimpleCosmos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+
+namespace Cosmos.Utility
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Azure Data Fest 2020 Attendees!");
+
+            string endpoint = "'your-cosmosdb-host'";
+            string authKey = "'your-unique-key-from-cosmosdb'";
+            
+            CosmosClient client = new CosmosClient(endpoint, authKey);
+            await client.ReadAccountAsync();
+
+
+            Console.WriteLine("End of demo, press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
